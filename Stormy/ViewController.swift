cb54a6fa32628e00e9f7528c0a39a0d3292d30df
Sean Kelley <s.kelley27@gmail.com>
2015-03-28 11:03:34 -0500

    App setup, still needs to display a loading screen until the network call is finished downloading. Still needs to dynamically determine location.

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -9,15 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var precipLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    private let apiKey = "4a7a86784dd76767baf4435021887aa1"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
+        let forecastURL = NSURL(string: "43.726596,-87.713287", relativeToURL: baseURL)
+        let sharedSession = NSURLSession.sharedSession()
+        let downloadTask: NSURLSessionDownloadTask = sharedSession.downloadTaskWithURL(forecastURL!, completionHandler:
+            {(location: NSURL!, response: NSURLResponse!, error: NSError!) -> Void in
+                if(error == nil){
+                    let dataObject = NSData(contentsOfURL: location)
+                    let weatherDictionary: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataObject!, options: nil, error: nil) as NSDictionary
+                    let currentWeather = Current(weatherDictionary: weatherDictionary)
+                    self.loadCurrentToScreen(currentWeather)
+                }
+        })
+        downloadTask.resume()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func loadCurrentToScreen(currentWeather: Current){
+        iconImage.image = currentWeather.icon
+        timeLabel.text = "At \(currentWeather.currentTime!) it is"
+        tempLabel.text = "\(currentWeather.temperature)"
+        humidityLabel.text = "\(Int(currentWeather.humidity * 100))%"
+        precipLabel.text = "\(Int(currentWeather.precipProbability * 100))%"
+        descriptionLabel.text = currentWeather.summary
     }
 
 
